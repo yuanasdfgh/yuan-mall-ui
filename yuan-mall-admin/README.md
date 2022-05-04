d6b055675735d8c17a765e2dca6815c0afdf5079
yuan <yuanasdfgh@gmail.com>
2022-05-04 19:48:31 +0800

    init

---
+++ yuan-mall-admin/README.md
@@ -0,0 +1,6 @@
+# Vue 3 + TypeScript + Vite
+
+## vite中文网脚手架项目搭建：https://vitejs.cn
+
+
+  pnpm run dev
---
+++ yuan-mall-admin/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    //设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [vue()],
+  server: {
+    port: 6666, //启动端口
+  }
+});
